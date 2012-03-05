1024937aa6b782aec96dd026fb8b3dca34cf556f
Rafael Souza <me@rafaelss.com>
2012-03-04 22:24:20 -0300

    add gemrc

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 ln -si $PWD/bash_profile ~/.bash_profile
 ln -si $PWD/gitconfig ~/.gitconfig
+ln -si $PWD/gemrc ~/.gemrc
 ln -si $PWD/sublime_preferences ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
