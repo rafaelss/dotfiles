65246ff9e5600e9ca3f5d0990754e0acee64fefb
Rafael Souza <me@rafaelss.com>
2012-03-06 18:25:52 -0300

    add sublime keymap file

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -si $PWD/gitconfig ~/.gitconfig
 ln -si $PWD/gemrc ~/.gemrc
 ln -si $PWD/inputrc ~/.inputrc
 ln -si $PWD/sublime_preferences ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
+ln -si $PWD/sublime_keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
