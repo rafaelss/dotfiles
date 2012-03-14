61cde75bd71a153c4afc82293f22b5dd39492d1d
Rafael Souza <me@rafaelss.com>
2012-03-14 00:36:02 -0300

    add irbrc

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -si $PWD/bash_profile ~/.bash_profile
 ln -si $PWD/gitconfig ~/.gitconfig
 ln -si $PWD/gemrc ~/.gemrc
+ln -si $PWD/irbrc ~/.irbrc
 ln -si $PWD/inputrc ~/.inputrc
 ln -si $PWD/sublime_preferences ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 ln -si $PWD/sublime_keymap ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Default\ \(OSX\).sublime-keymap
