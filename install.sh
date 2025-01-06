40a6d6045fa71b847fed63ac0df992a20557d84e
rafaelss <rafael.ssouza@gmail.com>
2025-01-06 12:17:39 -0300

    add scripts for basic zsh setup

--- install.sh
+++ install.sh
@@ -4,11 +4,15 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+zsh -c 'source $HOME/.zshrc; ln -nfs $PWD/zsh/envs.zsh $ZSH_CUSTOM/envs.zsh'
+
 if [ ! -d /opt/homebrew ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew bundle --file=$PWD/Brewfile
+
+zsh -c 'source $HOME/.zshrc; ln -nfs $PWD/zsh/mise.zsh $ZSH_CUSTOM/mise.zsh'
 
 ln -nfs $PWD/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 ln -nfs $PWD/gitconfig $HOME/.gitconfig
